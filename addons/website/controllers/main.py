4df48bbf60d35c9f5db99bf0784fca3d5d289c71
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-13 20:44:12 -0700

    [FIX] website: KeyError: 'login_success'

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -94,7 +94,7 @@ class Website(Home):
     @http.route(website=True, auth="public")
     def web_login(self, redirect=None, *args, **kw):
         response = super(Website, self).web_login(redirect=redirect, *args, **kw)
-        if not redirect and request.params['login_success']:
+        if not redirect and 'login_success' in request.params:
             if request.env['res.users'].browse(request.uid).has_group('base.group_user'):
                 redirect = b'/web?' + request.httprequest.query_string
             else:
