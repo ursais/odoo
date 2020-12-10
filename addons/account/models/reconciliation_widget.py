5fdf24b22bc6cf646e12e1b71616832823a27efe
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-12-10 10:28:30 -0600

    [PATCH] account: Bank reconciliation

--- addons/account/models/reconciliation_widget.py
+++ addons/account/models/reconciliation_widget.py
@@ -449,10 +449,9 @@ class AccountReconciliation(models.AbstractModel):
         """
 
         domain_reconciliation = [
-            '&', '&',
+            '&',
             ('statement_line_id', '=', False),
-            ('account_id', 'in', aml_accounts),
-            ('payment_id', '<>', False)
+            ('account_id', 'in', aml_accounts)
         ]
 
         # default domain matching
