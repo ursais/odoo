22d0d9010ab5a45423cc800650b633c08e83b3c1
RLeeOSI <51208020+RLeeOSI@users.noreply.github.com>
2021-06-17 12:43:10 -0700

    [FIX] error on unlink mail.thread with no messages

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -288,7 +288,11 @@ class MailThread(models.AbstractModel):
     def unlink(self):
         """ Override unlink to delete messages and followers. This cannot be
         cascaded, because link is done through (res_model, res_id). """
-        self.env['mail.message'].search([('model', '=', self._name), ('res_id', 'in', self.ids)]).unlink()
+        messages = self.env['mail.message'].search(
+            [('model', '=', self._name), ('res_id', 'in', self.ids)]
+        )
+        if messages:
+            messages.unlink()
         res = super(MailThread, self).unlink()
         self.env['mail.followers'].sudo().search(
             [('res_model', '=', self._name), ('res_id', 'in', self.ids)]
