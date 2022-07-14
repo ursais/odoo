37658203cbec62d16dae7ac1b614c7c2e8240709
Murtaza Mithaiwala <murtaza.m.serpentcs@gmail.com>
2022-07-14 13:59:28 +0530

    [14.0][PVM][FIX] Account Lock Date Issue.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1810,12 +1810,12 @@ class AccountMove(models.Model):
     def _check_fiscalyear_lock_date(self):
         for move in self:
             lock_date = move.company_id._get_user_fiscal_lock_date()
-#            if move.date <= lock_date:
-#                if self.user_has_groups('account.group_account_manager'):
-#                    message = _("You cannot add/modify entries prior to and inclusive of the lock date %s.", format_date(self.env, lock_date))
-#                else:
-#                    message = _("You cannot add/modify entries prior to and inclusive of the lock date %s. Check the company settings or ask someone with the 'Adviser' role", format_date(self.env, lock_date))
-#                raise UserError(message)
+            if move.date <= lock_date:
+                if self.user_has_groups('account.group_account_manager'):
+                    message = _("You cannot add/modify entries prior to and inclusive of the lock date %s.", format_date(self.env, lock_date))
+                else:
+                    message = _("You cannot add/modify entries prior to and inclusive of the lock date %s. Check the company settings or ask someone with the 'Adviser' role", format_date(self.env, lock_date))
+                raise UserError(message)
         return True
 
     @api.constrains('move_type', 'journal_id')
