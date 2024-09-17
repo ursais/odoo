bad772e7103925d45e42b4ad71d88d8b06ef03eb
Antonio Yamuta <ayamuta@opensourceintegrators.com>
2024-09-17 14:46:27 -0700

    [IMP] Bumped up module mrp version

--- addons/mrp/__manifest__.py
+++ addons/mrp/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Manufacturing',
-    'version': '2.2',
+    'version': '2.3',
     'website': 'https://www.odoo.com/app/manufacturing',
     'category': 'Manufacturing/Manufacturing',
     'sequence': 55,
