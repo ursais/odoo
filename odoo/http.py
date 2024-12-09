cd0828b6238fe6c12c5eff7b5af6d9b03f44456e
agyamuta@github.com <agyamuta@github.com>
2024-12-09 10:26:28 -0800

    [PATCH] Fixed Patch for CSN's CloudCTI VOIP integration migrated from v12.

--- odoo/http.py
+++ odoo/http.py
@@ -2008,7 +2008,9 @@ class JsonRPCDispatcher(Dispatcher):
             werkzeug.exceptions.abort(Response("Invalid JSON-RPC data", status=400))
 
         self.request.params = dict(self.jsonrequest.get('params', {}), **args)
-
+        if not self.request.params and self.request.httprequest.path == "/cloudCTI/statusChange":
+            self.request.params = self.jsonrequest
+            
         if self.request.db:
             result = self.request.registry['ir.http']._dispatch(endpoint)
         else:
