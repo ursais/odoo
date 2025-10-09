28e021ad05e109b7eb7647df60ad98633d909c04
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-10-09 19:38:47 +0000

    Fix for ticket 67189: Submodule update
    Automated changes for ticket resolution

--- addons/analytic/static/tests/mock_server/mock_models/account_analytic_line.js
+++
@@ -1,5 +0,0 @@
-import { models } from "@web/../tests/web_test_helpers";
-
-export class AccountAnalyticLine extends models.ServerModel {
-    _name = "account.analytic.line";
-}
--- addons/project_stock_account/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import analytic_applicability
-from . import stock_move
-from . import stock_picking_type
